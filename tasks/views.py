6e5c81f7019152cf652cfbad76ad9912a1823fae
amitmodi06 <amitmodi06@gmail.com>
2023-12-06 00:15:53 +0000

    added logic to add the task

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 
 # Create your views here.
@@ -21,6 +21,26 @@ def remaining_tasks(request):
 
 
 def add_task(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        due_date = request.POST.get("due_date")
+        due_time = request.POST.get("due_time")
+        completed = False
+
+        if title != "" and due_date != "" and due_time != "":
+            task = Task(
+                title=title,
+                description=description,
+                due_date=due_date,
+                due_time=due_time,
+                completed=completed
+            )
+            task.save()
+            return redirect('home')
+    
+    else:
+        return render(request, 'add_task.html')
     return render(request, 'add_task.html')
 
 
